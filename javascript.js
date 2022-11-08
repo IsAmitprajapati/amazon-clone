2a8f4b81f17678761927a855a242d3e873043fe3
Amit prajapati <57350935+IsAmitprajapati@users.noreply.github.com>
2022-11-08 20:11:05 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -7,24 +7,41 @@ console.log(imgItem.length-1)
 let startSlider = 0
 let endSlider = (imgItem.length - 1) * 100  // 700
 
-slideBtnLeft.addEventListener("click",()=>{
+slideBtnLeft.addEventListener("click",handleLeftBtn)
+
+function handleLeftBtn(){
     if(startSlider < 0){
         startSlider = startSlider + 100;
     }
     imgItem.forEach(element =>{
         element.style.transform = `translateX(${startSlider}%)`;
     })
-})
-slideBtnRight.addEventListener("click",()=>{
+}
+
+slideBtnRight.addEventListener("click",handleRightBtn)
+
+function handleRightBtn(){
     if(startSlider >= -endSlider+100){
         startSlider = startSlider - 100;
     }
     imgItem.forEach(element =>{
         element.style.transform = `translateX(${startSlider}%)`;
     })
-    
-    
-})
+     
+}
+//render automatic
+function renderSlideAuto(){
+   
+    if(startSlider >= -endSlider+100){
+        handleRightBtn()
+    }
+    else{
+        startSlider  = 0 ;
+    }
+}
+setInterval(renderSlideAuto, 2000);
+
+
 
 
  /***** sidebar navigation  */
